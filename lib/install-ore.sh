b5327155044474ac7a6ed1840e26c5dcc2d2ca92
guenter hager <guenter@eb8.org>
2018-01-03 08:24:55 +0100

    ore-take1

---
+++ lib/install-ore.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# installation of Galactrum Coin 
+# wallet & daemon
+
+GIT=https://github.com/galactrum/galactrum.git
+BASE_NAME=`basename $GIT .git`
+COIN_NAME=Galactrum
+COIN_SIGN=ORE
+EXEC=/opt/$BASE_NAME/bin/galactrum-qt
+
+ICON=
+DESC_NAME="$COIN_NAME Wallet"
+DESC_COMMENT="$COIN_NAME Desktop Wallet"
+DESC_KEYWORDS="$COIN_NAME;$COIN_SIGN;Crypto-Currency"
+DESC_CATEGORIES=Network
+
+# Install from git
+cd $HOME
+
+if [ -d $BASE_NAME ]; then
+    cd $BASE_NAME
+    git pull
+else
+    git clone $GIT
+fi
+
+cd $BASE_NAME
+
+./autogen.sh
+./configure --prefix=/opt/$BASE_NAME
+make
+#sudo make install 
+
+exit 0
+# create desktop file
+cat << EOF > $COIN_NAME.desktop
+
+[Desktop Entry]
+Comment=$DESC_COMMENT
+Exec=$EXEC
+GenericName[en_US]=$DESC_NAME
+GenericName=$DESC_NAME
+Icon=
+Name[en_US]=$DESC_NAME
+Name=$DESC_NAME
+Categories=Network;
+StartupNotify=false
+Terminal=false
+Type=Application
+MimeType=x-scheme-handler/$COIN_NAME;
+EOF
+
+sudo desktop-file-install $COIN_NAME.desktop
+
